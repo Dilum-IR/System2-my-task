86bad6bec49d343dd95425000b29904469203e68
Dilum-IR <rdinduwara19158@gmail.com>
2024-11-20 09:57:57 +0530

    [fix] change the base URL

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -1,5 +1,5 @@
 // Base URL of the API
-const API_BASE_URL = "https://api-uhlqia5n5a-uc.a.run.app";
+const API_BASE_URL = "https://api-7waq6cxxaa-uc.a.run.app";
 
 // Login endpoint path
 export const LOGIN_URL = API_BASE_URL + "/api/v1/auth/login";
